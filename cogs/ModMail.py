1cc4e109f1777ba859e93f4e39432ab8f9bd6391
xrezut <31830718+xrezut@users.noreply.github.com>
2020-08-03 14:28:08 +0100

    Added comments, cleaned code, and now use JSON file to store channels. Also made setup easier.

--- cogs/ModMail.py
+++ cogs/ModMail.py
@@ -1,87 +1,122 @@
 import discord
 from discord.ext import commands
-import datetime
+from datetime import datetime
 import csv
+import json
 
 class ModMail(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.channels_dict = {}
+
+        try:
+            with open('channels.json') as json_file:
+                self.channels_dict = json.load(json_file)
+                self.bot.mod_mail_channel = self.channels_dict['open']
+                self.bot.resolved_mail_channel = self.channels_dict['resolved']
+        except KeyError as e:
+            print(e)
+
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
 
+        # Check if message is in DM and message author is not a Bot
         if message.guild is None and not message.author.bot:
-            mod_channel = self.bot.get_channel(int(self.bot.mod_mail_channel))
-            ts = datetime.datetime.now().timestamp()
+
+            # Channel must be in the cache. Otherwise use fetch_channel if channel is returned none.
+            mod_channel = self.bot.get_channel(self.bot.mod_mail_channel)
+
+            # Create and send the message as an embed to the mod mail channel
             embed = discord.Embed(title=f":e_mail: **New DM to Bot**", colour=discord.Colour.red(),
-                                  timestamp=datetime.datetime.utcfromtimestamp(ts))
+                                  timestamp=datetime.utcfromtimestamp(datetime.now().timestamp()))
             embed.add_field(name="__Message from:__", value=message.author, inline=False)
             embed.add_field(name="__Message__", value=message.content, inline=False)
-            message = await mod_channel.send(content=message.author.mention, embed=embed)
+
+            message = await mod_channel.send(content=f"Message by: {message.author.mention}", embed=embed)
+
+            # Add reactions to the message
             check_mark = '\N{WHITE HEAVY CHECK MARK}'
             cross_mark = '\N{CROSS MARK}'
             await message.add_reaction(check_mark)
             await message.add_reaction(cross_mark)
 
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        print(payload)
-        mail_channel_id = int(self.bot.mod_mail_channel)
-        resolved_channel_id = int(self.bot.resolved_mail_channel)
+
         guild = self.bot.get_guild(payload.guild_id)
 
-        member = guild.get_member(payload.user_id)
-        resolved_channel = guild.get_channel(resolved_channel_id)
-        mod_channel = guild.get_channel(mail_channel_id)
+        # Get the mail channels
+        resolved_channel = guild.get_channel(self.bot.resolved_mail_channel)
+        mod_channel = guild.get_channel(self.bot.mod_mail_channel)
 
-        if payload.channel_id == mail_channel_id:
+        # Check if the channel where reaction was made was the mod mail channel
+        if payload.channel_id == self.bot.mod_mail_channel:
             message = await mod_channel.fetch_message(payload.message_id)
+
+            # If user reacts with check mark
             if message.reactions[0].count == 2:
                 if payload.emoji.name == '✅':
                     if payload.user_id != self.bot.user.id:
+
+                        # Edit the embed to say resolved and then send.
                         embed = message.embeds[0]
                         embed.add_field(name="Status", value=f"Resolved by {payload.member.name}", inline=False)
                         msg = await resolved_channel.send(embed=embed)
 
+                        # Add the reactions again.
                         cross_mark = '\N{CROSS MARK}'
                         await msg.add_reaction(cross_mark)
 
+                        # Delete the old embed
                         await message.delete()
 
+            # If user reacts with cross
             if message.reactions[1].count == 2:
                 if payload.emoji.name == '❌':
                     if payload.user_id != self.bot.user.id:
+
+                        # Edit the embed to say cancelled and then send.
                         embed = message.embeds[0]
                         embed.add_field(name="Status", value=f"Cancelled by {payload.member.name}", inline=False)
                         msg = await resolved_channel.send(embed=embed)
 
+                        # Add the reactions again.
                         cross_mark = '\N{CROSS MARK}'
                         await msg.add_reaction(cross_mark)
 
+                        # Delete the old message
                         await message.delete()
 
-
-        if payload.channel_id == resolved_channel_id:
+        # Check if the channel where reaction was made was the resolved mail channel
+        if payload.channel_id == self.bot.resolved_mail_channel:
             message = await resolved_channel.fetch_message(payload.message_id)
+
+            # If user reacts with cross mark to indicate they want to reopen a resolved or cancelled ticket
             if message.reactions[0].count == 2:
                 if payload.emoji.name == '❌':
                     if payload.user_id != self.bot.user.id:
+
+                        # Get the embed and edit to say reopened ticket
                         embed = message.embeds[0]
                         embed.add_field(name="Status", value=f"Reopened by {payload.member.name}", inline=False)
                         msg = await mod_channel.send(embed=embed)
 
+                        # Add the reactions again.
                         check_mark = '\N{WHITE HEAVY CHECK MARK}'
                         cross_mark = '\N{CROSS MARK}'
                         await msg.add_reaction(check_mark)
                         await msg.add_reaction(cross_mark)
 
+                        # Delete the old message.
                         await message.delete()
 
 
     @commands.command()
     async def help(self, ctx):
+        """Sends the help message"""
+
         embed = discord.Embed(title=f"Set Up Instructions", description="This bot only contains one command.",
                               colour=discord.Colour.red())
         embed.add_field(name=f"Setup Command",
@@ -94,23 +129,28 @@ class ModMail(commands.Cog):
                               f"A new mail message can be cancelled using the :x: reaction. Both these reactions will send the "
                               f"mail to the resolved channel, and mark the status of it as resolved. A resolved query can be opened "
                               f"again using the :x: reaction from the resolved channel.", inline=False)
+
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def setup(self, ctx, open_queries, resolved_queries):
-        fields = [open_queries, resolved_queries]
+    async def setup(self, ctx, open_queries: discord.TextChannel, resolved_queries: discord.TextChannel):
+        """Command used to setup the mod mail channels"""
+
+        channels_dict = {'open': open_queries.id, 'resolved': resolved_queries.id}
+
         try:
-            with open("channels.csv", 'w', newline='', encoding="utf-8") as fp:
-                writer = csv.writer(fp, delimiter=",")
-                writer.writerow(fields)
-            self.bot.mod_mail_channel = open_queries
-            self.bot.resolved_mail_channel = resolved_queries
-            embed = discord.Embed(title=f"Setup Successful", description=f"Mail Channel: {open_queries}\n"
-                                                                         f"Resolved Channel: {resolved_queries}",
+            self.bot.mod_mail_channel = open_queries.id
+            self.bot.resolved_mail_channel = resolved_queries.id
+            embed = discord.Embed(title=f"Setup Successful", description=f"Mail Channel: {open_queries.mention}\n"
+                                                                         f"Resolved Channel: {resolved_queries.mention}",
                                   colour=discord.Colour.red())
             await ctx.send(embed=embed)
+
+            with open('channels.json', 'w') as fp:
+                json.dump(channels_dict, fp)
         except Exception as e:
             print(e)
+
 
     @setup.error
     async def setup_error(self, ctx, error):
@@ -119,6 +159,14 @@ class ModMail(commands.Cog):
                                   colour=discord.Colour.red())
             embed.add_field(name=":grey_question: Syntax",
                             value=f"[!setup <open mail channel ID> <resolved mail channel ID>](https://discord.com)")
+            embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url_as(format="png", size=1024))
+            await ctx.send(embed=embed)
+
+        if isinstance(error, commands.BadArgument):
+            embed = discord.Embed(title=":x: Oops!", description="One of the channels you entered does not exist.",
+                                  colour=discord.Colour.red())
+            embed.add_field(name=":grey_question: Syntax",
+                            value=f"[!setup <open mail channel> <resolved mail channel>](https://discord.com)")
             embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url_as(format="png", size=1024))
             await ctx.send(embed=embed)
 
--- main.py
+++ main.py
@@ -9,23 +9,13 @@ load_dotenv()
 prefix = os.getenv("PREFIX")
 token = os.getenv("TOKEN")
 
-client = commands.Bot(command_prefix=prefix, case_insensitive=True)
+client = commands.Bot(command_prefix=prefix, case_insensitive=True, help_command=None)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     activity = discord.Activity(name='my DMs', type=discord.ActivityType.watching)
     await client.change_presence(activity=activity)
-
-    csv_file = csv.reader(open('channels.csv', "rt", encoding="utf-8"), delimiter=",")
-
-    for row in csv_file:
-        client.mod_mail_channel = row[0]
-        client.resolved_mail_channel = row[1]
-        break
-
-client.remove_command("help")
-
 
 extensions = ['cogs.ModMail']
 
